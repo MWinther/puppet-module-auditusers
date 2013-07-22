e5768c519002bade81de0c92b8d0559f778514c0
Garrett Honeycutt <code@garretthoneycutt.com>
2013-07-22 14:33:56 -0700

    Fix style issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,23 +3,23 @@
 # Full description of class module-auditusers here.
 #
 class auditusers (
-  $basedir = '/opt/auditusers',
-  $bindir = 'bin',
-  $script_name = 'auditscript.sh',
-  $user = 'audituser',
-  $uid = '9000',
-  $domain = 'example.com',
-  $primary_group = 'auditgroup',
-  $groups = undef,
+  $basedir           = '/opt/auditusers',
+  $bindir            = 'bin',
+  $script_name       = 'auditscript.sh',
+  $user              = 'audituser',
+  $uid               = '9000',
+  $domain            = 'example.com',
+  $primary_group     = 'auditgroup',
+  $groups            = undef,
   $groups_membership = 'minimum',
-  $gid = '8000',
-  $users_allow = '/etc/users.allow',
-  $cron_minute = 'auto',
-  $report_vol = '/var/run/auditusers',
-  $fstab_entry = undef,
-  $mount_report_vol = true,
-  $report_dir = 'incoming',
-  $hub = 'hub',
+  $gid               = '8000',
+  $users_allow       = '/etc/users.allow',
+  $cron_minute       = 'auto',
+  $report_vol        = '/var/run/auditusers',
+  $fstab_entry       = undef,
+  $mount_report_vol  = true,
+  $report_dir        = 'incoming',
+  $hub               = 'hub',
 ) {
 
   if $groups == undef {
@@ -36,7 +36,7 @@ class auditusers (
   } elsif $groups_membership == 'inclusive' {
     $the_groups_membership = $groups_membership
   } else {
-    fail("auditusers::groups_membership can have either of the values 'minimum' or 'inclusive'. groups_membership is currently set to $groups_membership.")
+    fail("auditusers::groups_membership can have either of the values 'minimum' or 'inclusive'. groups_membership is currently set to ${groups_membership}.")
   }
 
   if $cron_minute == 'auto' {
@@ -67,39 +67,39 @@ class auditusers (
     $fstab_entry_type = type($fstab_entry)
     if $fstab_entry_type == 'hash' {
       $my_fstab_entry = {'the_mount' => $fstab_entry, }
-      $mount_defaults = {'name'      => $report_vol,
-                         'ensure'    => $mount_report_vol_ensure,}
+      $mount_defaults = { 'name'      => $report_vol,
+                          'ensure'    => $mount_report_vol_ensure, }
     } else {
-      fail("fstab entry can either be a hash or undefined. fstab_entry is defined as $fstab_entry_type.")
+      fail("fstab entry can either be a hash or undefined. fstab_entry is defined as ${fstab_entry_type}.")
     }
   }
 
   file { 'basedir':
-    name   => $basedir,
     ensure => directory,
-    mode   => 0755,
+    name   => $basedir,
     owner  => 'root',
     group  => 'root',
+    mode   => '0755',
   }
 
   file { 'bindir':
-    name   => "${basedir}/${bindir}",
     ensure => directory,
-    mode   => 0755,
+    name   => "${basedir}/${bindir}",
     owner  => 'root',
     group  => 'root',
+    mode   => '0755',
   }
 
   exec { 'add_to_users.allow':
     path    => '/usr/xpg4/bin:/bin:/usr/bin:/sbin:/usr/sbin',
-    command => "echo ${user}@${domain} >> $users_allow",
-    onlyif  => "test -f $users_allow",
-    unless  => "grep -q ${user}@${domain} $users_allow",
+    command => "echo ${user}@${domain} >> ${users_allow}",
+    onlyif  => "test -f ${users_allow}",
+    unless  => "grep -q ${user}@${domain} ${users_allow}",
   }
 
   group { 'primary_group':
-    name    => $primary_group,
     ensure  => present,
+    name    => $primary_group,
     gid     => $gid,
     require => Exec['add_to_users.allow'],
   }
@@ -109,8 +109,8 @@ class auditusers (
   }
 
   user { 'audit_user':
-    name       => $user,
     ensure     => present,
+    name       => $user,
     uid        => $uid,
     gid        => $primary_group,
     groups     => $the_group_list,
@@ -119,14 +119,15 @@ class auditusers (
   }
 
   file { 'audit_script':
-    path    => "${basedir}/${bindir}/${script_name}",
     ensure  => present,
-    mode    => 0750,
+    path    => "${basedir}/${bindir}/${script_name}",
     owner   => 'root',
     group   => $primary_group,
+    mode    => '0750',
     source  => "puppet:///modules/auditusers/${script_name}",
-    require => [File['bindir'],
-                User['audit_user']],
+    require => [ File['bindir'],
+                User['audit_user'],
+                ],
   }
 
   if $fstab_entry != undef {
@@ -134,31 +135,29 @@ class auditusers (
     if $mount_report_vol_ensure == 'mounted' {
 
       file { 'report_vol':
-        path   => $report_vol,
         ensure => 'directory',
+        path   => $report_vol,
         before => Mount['the_mount'],
       }
 
     } else {
 
       file { 'report_vol':
-        path    => $report_vol,
         ensure  => 'absent',
+        path    => $report_vol,
         force   => true,
         require => Mount['the_mount'],
       }
-
     }
 
     create_resources(mount, $my_fstab_entry, $mount_defaults)
   }
 
   cron { 'count_users':
-    command => "${basedir}/${bindir}/${script_name} ${report_vol}/${report_dir} $hub",
     ensure  => $cron_ensure,
+    command => "${basedir}/${bindir}/${script_name} ${report_vol}/${report_dir} ${hub}",
     user    => $user,
     minute  => $the_minute,
     require => File['audit_script'],
   }
-
 }
